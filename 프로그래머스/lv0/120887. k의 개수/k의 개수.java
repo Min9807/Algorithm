4c7751e340171860af69d90d960956917478ccc2
yeondol <97437403+Min9807@users.noreply.github.com>
2023-03-20 16:54:07 +0900

    [level 0] Title: k의 개수, Time: 66.21 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        for(int num=i; num<=j; num++){
+            String[] a = String.valueOf(num).split("");
+            for(int b=0; b<a.length; b++){
+                if(String.valueOf(k).equals(a[b])){
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
