2fe44a114f13687596d309925a9c28af11d12706
yeondol <97437403+Min9807@users.noreply.github.com>
2023-03-20 16:52:36 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        int count = 0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        answer = new int[count];
+        int j=0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                answer[j] = i;
+                j++;
+            }
+        }
+        return answer;
+    }
+}
