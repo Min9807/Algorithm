b83f448cce06a4fdbbc3e94fa7a22e1f725c7424
yeondol <97437403+Min9807@users.noreply.github.com>
2023-03-20 16:54:16 +0900

    [level 0] Title: 가까운 수, Time: 0.02 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        int count = 0;
+        for(int i=0; i<array.length; i++){
+            if(i==0){
+                if(n>=array[i]){
+                    count = n-array[i];
+                    answer = array[i];
+                }
+                else{
+                    count = array[i] - n;
+                    answer = array[i];
+                }
+            }
+            if(n>array[i]){
+                if(count>=n-array[i]){
+                    count = n-array[i];
+                    answer = array[i];
+                }
+
+            }
+            else{
+                if(count>array[i]-n){
+                    count = array[i]-n;
+                    answer = array[i];
+                }
+            }
+        }
+        return answer;
+    }
+}
