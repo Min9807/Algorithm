df903db2b856adcb7e233583ec61d1483c6f618b
yeondol <97437403+Min9807@users.noreply.github.com>
2023-04-09 14:40:48 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.19 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,19 +1,28 @@
 import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
-        int[] answer = new int[score.length];
-        List<Integer> sc = new ArrayList();
-        for (int i=0; i<score.length; i++) {
-            if (i<k-1){
-                sc.add(score[i]);
-                sc.sort(Collections.reverseOrder());
-                answer[i]=sc.get(sc.size()-1);
-            } else if (i>=k-1){
-                sc.add(score[i]);
-                sc.sort(Collections.reverseOrder());
-                answer[i]=sc.get(k-1);
+        int len = score.length;
+        int[] answer = new int[len];
+        
+        List<Integer> stack = new ArrayList<>();
+        
+        for(int i=0; i<len; i++){
+            if(stack.size()==k){
+                if(stack.get(0)<score[i]){
+                    stack.add(score[i]);
+                    stack.remove(0);
+                    Collections.sort(stack);
+                }
             }
+            else{
+                stack.add(score[i]);
+                Collections.sort(stack);
+            }
+            
+            
+            answer[i] = stack.get(0);
         }
+        
         return answer;
     }
 }
