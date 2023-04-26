af3f5565df435e72d0f0b61bd6a2dca7853108ff
yeondol <97437403+Min9807@users.noreply.github.com>
2023-04-26 10:56:22 +0900

    [level 1] Title: 모의고사, Time: 0.74 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,35 +1,55 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-        int[] count = {0, 0, 0};
-        int[] one = {1, 2, 3, 4, 5};
-        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        List<Integer> answer = new ArrayList<>();
         
-        for(int i=0; i<answers.length; i++){
-            if(answers[i] == one[i%5]) count[0]++;
-            if(answers[i] == two[i%8]) count[1]++;
-            if(answers[i] == three[i%10]) count[2]++;
-        }
+        int[] studentA = {1,2,3,4,5};
+        int[] studentB = {2,1,2,3,2,4,2,5};
+        int[] studentC = {3,3,1,1,2,2,4,4,5,5};
         
-        int max = Math.max(count[0], Math.max(count[1], count[2]));
-        
-        List<Integer> num = new ArrayList<>();
-        
-        for(int i=0; i<3; i++){
-            if(count[i] == max){
-                num.add(i+1);
-            }
-        }
-        
-        int[] answer = new int[num.size()];
-        
-        for(int i=0; i<num.size(); i++){
-            answer[i] = num.get(i);
-        }
-        
-        
-        return answer;
-    }
+        int studentACount = calcu(studentA, answers);
+        int studentBCount = calcu(studentB, answers);
+        int studentCCount = calcu(studentC, answers);
 
+        answer.add(1);
+        int max = studentACount;
+
+        if(studentBCount > max){
+            answer.clear();
+            max = studentBCount;
+            answer.add(2);
+        }
+        else if(studentBCount == max){
+            answer.add(2);
+        }
+        
+        if(studentCCount > max){
+            answer.clear();
+            answer.add(3);
+        }
+        else if(studentCCount == max){
+            answer.add(3);
+        }
+        
+        int[] result = new int[answer.size()];
+        for(int i=0; i<result.length; i++){
+            result[i] = answer.get(i);
+        }
+        
+        return result;
+    }
+    public int calcu(int[] student, int[] answers){
+        int count = 0;
+        int n = 0;
+        for(int answer : answers){
+            if(count == student.length){
+                count = 0;
+            }
+            if(answer == student[count]){
+                n++;
+            }
+            count++;
+        }
+        return n;
+    }
 }
