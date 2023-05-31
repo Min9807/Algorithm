03296041529443ad2596922e999a3a58c813e107
yeondol <97437403+Min9807@users.noreply.github.com>
2023-05-31 19:27:01 +0900

    [level 1] Title: 시저 암호, Time: 19.65 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,39 +1,23 @@
 class Solution {
     public String solution(String s, int n) {
         String answer = "";
-        String[] str = s.split("");
-        for(int i=0; i<str.length; i++){
-            if(str[i].equals(" ")){ // 공백일 때
+
+        for(char ch : s.toCharArray()){
+            int k = ch + n;
+            if(ch >= 65 && ch<=90){ // 대문자인 경우.
+                if(k>90){
+                    k -= 26;
+                }
+            } else if(ch >= 90 && ch <= 122){ // 소문자인 경우.
+                if(k>122){
+                    k -= 26;
+                }
+            } else{
+                answer += " ";
                 continue;
             }
-            
-            else if(str[i].charAt(0)>=65 && str[i].charAt(0)<=90){ // 대문자인 경우
-                if((str[i].charAt(0) + n) >90){ // Z를 넘는 경우
-                    int num = 64+(str[i].charAt(0) + n)-90;
-                    str[i] = String.valueOf((char)num);
-
-                }else{
-                    int num = str[i].charAt(0) + n;
-                    str[i] = String.valueOf((char)num);
-                }
-            }
-            
-            else if(str[i].charAt(0)>=97 && str[i].charAt(0)<=122){ // 소문자인 경우
-                if((str[i].charAt(0) + n) >122){ // z를 넘는 경우
-                    int num = 96+(str[i].charAt(0) + n)-122;
-                    str[i] = String.valueOf((char)(num));
-
-                }else{
-                    int num = str[i].charAt(0) + n;
-                    str[i] = String.valueOf((char)num);
-                }
-            }
+            answer += String.valueOf((char)k);
         }
-        
-        for(String a : str){
-            answer+=a;
-        }
-        
         return answer;
     }
 }
