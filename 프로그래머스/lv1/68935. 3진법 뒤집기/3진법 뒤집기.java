d3e6d84525491da216db4e5a695c44b56be6bbeb
yeondol <97437403+Min9807@users.noreply.github.com>
2023-06-01 00:28:16 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,17 +1,8 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        String[] num = Integer.toString(n, 3).split("");
-        for(int i=0; i<num.length/2; i++){
-            String temp =num[i];
-            num[i] = num[num.length-1-i];
-            num[num.length-1-i] = temp;
-        }
-        String number="";
-        for(String a : num){
-            number+=a;
-        }
-        answer = Integer.parseInt(number, 3);
-        return answer;
+        StringBuilder sb = new StringBuilder(Integer.toString(n, 3));
+        sb.reverse();
+        
+        return Integer.parseInt(sb.toString(), 3);
     }
 }
