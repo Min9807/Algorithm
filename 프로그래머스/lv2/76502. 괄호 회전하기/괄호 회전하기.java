add8d1d8b5ebf1041448f364e1ff70246576784d
yeondol <97437403+Min9807@users.noreply.github.com>
2023-06-04 21:25:38 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.49 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,58 +2,31 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        
         for(int i=0; i<s.length(); i++){
-            String a = s.substring(i,s.length()) + s.substring(0,i);
-            
-            if(check(a)){
+            String str = s.substring(i) + s.substring(0,i);
+            if(check(str)){
                 answer++;
             }
         }
+        
         return answer;
     }
-    public boolean check(String str){
+    
+    private boolean check(String str){
         Stack<Character> st = new Stack<>();
         
-        for(int i=0; i<str.length(); i++){
-            if(str.charAt(i)=='(' || str.charAt(i)=='{' || str.charAt(i)=='['){
-                st.push(str.charAt(i));
-            }                
-                
-            else if(str.charAt(i)==')' || str.charAt(i)=='}' || str.charAt(i)==']'){
-                if(st.empty()){
-                    return false;
-                }
-                switch(st.peek()){
-                    case '{':
-                        if('}' == str.charAt(i)) break;
-                        else{
-                            return false;
-                        }
-                    case '[':
-                        if(']' == str.charAt(i)) break;
-                        else{
-                            return false;
-                        }
-                    case '(':
-                        if(')' == str.charAt(i)) break;
-                        else{
-                            return false;
-                        }
-                }
-
-                st.pop();
-            }
+        for(char c : str.toCharArray()){
+            if(c == '[' || c == '{' || c == '(') st.push(c);
+            else if(st.isEmpty() && (c == ']' || c == ')' || c == '}')) return false;
+            else if(st.peek() == '(' && c == ')') st.pop();
+            else if(st.peek() == '[' && c == ']') st.pop();
+            else if(st.peek() == '{' && c == '}') st.pop();
             
-            else{
-                return false;
-            }
         }
         
+        if(!st.isEmpty()) return false;
         
-        if(!st.empty()){
-            return false;
-        }
-        
-        return true; 
+        return true;
     }
 }
