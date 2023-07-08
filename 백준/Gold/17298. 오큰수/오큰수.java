32b4985584a10ca9cde83cd0169e98218efd77ea
yeondol <97437403+Min9807@users.noreply.github.com>
2023-07-08 13:50:32 +0900

    [Gold IV] Title: 오큰수, Time: 1008 ms, Memory: 262408 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int[] B = new int[N];
+        Arrays.fill(B, -1);  // B 배열을 -1로 초기화
+
+        StringTokenizer stz = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<N; i++){
+            A[i] = Integer.parseInt(stz.nextToken());
+        }
+
+        Stack<Integer> st = new Stack<>();
+        for (int i=0; i<N; i++) {
+            while (!st.empty() && A[st.peek()] < A[i]) {
+                B[st.pop()] = A[i];
+            }
+            st.push(i);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int num : B){
+            sb.append(num + " ");
+        }
+        bw.write(sb.toString().trim());
+        bw.flush();
+    }
+}
