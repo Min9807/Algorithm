f49e25c2d99e97457e236f0c5d2f9c7aa6c95874
yeondol <97437403+Min9807@users.noreply.github.com>
2023-07-08 14:15:55 +0900

    [Gold IV] Title: 오큰수, Time: 1100 ms, Memory: 183488 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -11,20 +11,20 @@ public class Main {
         Arrays.fill(B, -1);  // B 배열을 -1로 초기화
 
         StringTokenizer stz = new StringTokenizer(br.readLine(), " ");
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(stz.nextToken());
         }
 
         Stack<Integer> st = new Stack<>();
-        for (int i=0; i<N; i++) {
-            while (!st.empty() && A[st.peek()] < A[i]) {
+        for(int i=0; i<N; i++){
+            while(!st.empty() && A[st.peek()] < A[i]){
                 B[st.pop()] = A[i];
             }
             st.push(i);
         }
-        
+
         StringBuilder sb = new StringBuilder();
-        for(int num : B){
+        for (int num : B) {
             sb.append(num + " ");
         }
         bw.write(sb.toString().trim());
