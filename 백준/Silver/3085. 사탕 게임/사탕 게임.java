5ccd8b32987402d08df97cc2fcced615c98f598e
yeondol <97437403+Min9807@users.noreply.github.com>
2023-07-17 00:07:57 +0900

    [Silver II] Title: 사탕 게임, Time: 268 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -1,66 +1,68 @@
-import java.util.Scanner;
+import java.util.*;
 
-class Main
-{
-    public static int calcScore(char[][] map) {
+public class Main {
+    public static int findMaxRow(char[][] map){
         int N = map.length;
-        int maxScore = 0;
-        // 가장 긴 연속 column 길이 찾기
-        for (int r = 0; r < N; r++) {
-            int scr = 1;
-            for (int c = 1; c < N; c++) {
-                if (map[r][c] == map[r][c - 1]) scr++;
-                else {
-                    maxScore = Math.max(maxScore, scr);
-                    scr = 1;
+        int maxRow = 0;
+        for(int r=0; r<N; r++){
+            int len = 1;
+            for(int c=1; c<N; c++){
+                if(map[r][c] == map[r][c-1]) len++;
+                else{
+                    maxRow = Math.max(maxRow, len);
+                    len = 1;
                 }
             }
-            maxScore = Math.max(maxScore, scr);
+            maxRow = Math.max(maxRow, len);
         }
-        // 가장 긴 연속 row 길이 찾기
-        for (int c = 0; c < N; c++) {
-            int scr = 1;
-            for (int r = 1; r < N; r++) {
-                if (map[r][c] == map[r - 1][c]) scr++;
-                else {
-                    maxScore = Math.max(maxScore, scr);
-                    scr = 1;
-                }
-            }
-            maxScore = Math.max(maxScore, scr);
-        }
-        return maxScore;
+        return maxRow;
     }
 
-    public static void swapCandy(char[][] map, int r1, int c1, int r2, int c2) {
-        char tmp = map[r1][c1];
+    public static int findMaxColumn(char[][] map){
+        int N = map.length;
+        int maxColumn = 0;
+        for(int c=0; c<N; c++){
+            int len = 1;
+            for(int r=1; r<N; r++){
+                if(map[r][c] == map[r-1][c]) len++;
+                else{
+                    maxColumn = Math.max(maxColumn, len);
+                    len = 1;
+                }
+            }
+            maxColumn = Math.max(maxColumn, len);
+        }
+        return maxColumn;
+    }
+
+    public static void swapCandy(char[][] map, int r1, int c1, int r2, int c2){
+        char temp = map[r1][c1];
         map[r1][c1] = map[r2][c2];
-        map[r2][c2] = tmp;
+        map[r2][c2] = temp;
     }
-
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        int max = 0;
         char[][] map = new char[N][N];
-        for (int i = 0; i < N; i++)
-            map[i] = sc.next().toCharArray();
+        for(int i=0; i<N; i++) map[i] = sc.next().toCharArray();
 
         int ans = 0;
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++){
-                // swap right
-                if (j + 1 < N) {
-                    swapCandy(map, i, j, i, j + 1);
-                    ans = Math.max(ans, calcScore(map));
-                    swapCandy(map, i, j, i, j + 1);
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                if(j+1 < N && map[i][j] != map[i][j+1]){
+                    swapCandy(map, i, j, i, j+1);
+                    ans = Math.max(ans, Math.max(findMaxColumn(map), findMaxRow(map)));
+                    swapCandy(map, i, j, i, j+1);
                 }
-                // swap below
-                if (i + 1 < N) {
-                    swapCandy(map, i, j, i + 1, j);
-                    ans = Math.max(ans, calcScore(map));
-                    swapCandy(map, i, j, i + 1, j);
+                if(i+1 < N && map[i][j] != map[i+1][j]){
+                    swapCandy(map, i, j, i+1, j);
+                    ans = Math.max(ans, Math.max(findMaxColumn(map), findMaxRow(map)));
+                    swapCandy(map, i, j, i+1, j);
                 }
             }
+        }
         System.out.println(ans);
+
     }
 }
