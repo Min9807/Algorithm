ff504108eb437225fa169f9bbff66184b0227920
yeondol <97437403+Min9807@users.noreply.github.com>
2023-07-23 16:41:53 +0900

    [level 2] Title: 타겟 넘버, Time: 0.48 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,35 +1,19 @@
-import java.util.*;
-
 class Solution {
+    int answer = 0;
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        Stack<State> st = new Stack<>();
         
-        st.push(new State(0, 0));
-        
-        while(!st.isEmpty()){
-            State state = st.pop();
-            
-            if(state.index == numbers.length){
-                if(state.acc == target){
-                    answer++;
-                }
-                continue;
-            }
-            st.push(new State(state.index + 1, state.acc - numbers[state.index]));
-            st.push(new State(state.index + 1, state.acc + numbers[state.index]));
-        }
-        
+        dfs(numbers, target, 0, 0);
         return answer;
     }
     
-    private static class State{
-        public final int index;
-        public final int acc;
-        
-        public State(int index, int acc){
-            this.index = index;
-            this.acc = acc;
+    public void dfs(int[] numbers, int target, int len, int sum){
+        if(len == numbers.length){
+            if(sum == target){
+                answer++;
+            }
+        }else{
+            dfs(numbers, target, len + 1, sum + numbers[len]);
+            dfs(numbers, target, len + 1, sum - numbers[len]);
         }
     }
 }
