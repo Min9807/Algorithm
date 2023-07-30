b554f1af6ce05ba41d4fe3e16c73646513d1e497
MinJeong <97437403+Min9807@users.noreply.github.com>
2023-07-30 18:12:54 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1896 ms, Memory: 99168 KB -BaekjoonHub

--- 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -2,10 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] graphs;
+    static ArrayList<Integer>[] graph;
     static boolean[] visited;
     static int[] order;
     static int cnt = 1;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,43 +15,42 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
 
-        graphs = new ArrayList[N+1];
-        for(int i=1; i<=N; i++){
-            graphs[i] = new ArrayList<>();
-        }
-
+        graph = new ArrayList[N+1];
         visited = new boolean[N+1];
         order = new int[N+1];
-
+        for(int i=1; i<=N; i++){
+            graph[i] = new ArrayList<>();
+        }
+        
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            graphs[a].add(b);
-            graphs[b].add(a);
+            
+            graph[a].add(b);
+            graph[b].add(a);
         }
-
         for(int i=1; i<=N; i++){
-            Collections.sort(graphs[i], Collections.reverseOrder());
+            Collections.sort(graph[i], Collections.reverseOrder());
         }
-
+        
         bfs(R);
-
+        
         for(int i=1; i<=N; i++){
             System.out.println(order[i]);
         }
-    }
 
+    }
+    
     private static void bfs(int start){
         Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
         visited[start] = true;
         order[start] = cnt++;
-        q.offer(start);
-
+        
         while(!q.isEmpty()){
-            int current = q.poll();
-
-            for(int next : graphs[current]){
+            int num = q.poll();
+            for(int next : graph[num]){
                 if(!visited[next]){
                     visited[next] = true;
                     order[next] = cnt++;
@@ -59,4 +59,5 @@ public class Main {
             }
         }
     }
+
 }
