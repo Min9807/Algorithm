8cfbacf333a3d14c58888b32de81aab9270fbb57
MinJeong <97437403+Min9807@users.noreply.github.com>
2023-08-01 17:25:20 +0900

    [Silver I] Title: 회의실 배정, Time: 636 ms, Memory: 46208 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -4,42 +4,42 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        Meet[] meets = new Meet[N];
+        int ans = 1;
+        Node[] graph = new Node[N];
         int idx = 0;
-
         while(N-- > 0){
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            meets[idx++] = new Meet(start, end);
+            graph[idx++] = new Node(start, end);
         }
-        Arrays.sort(meets, (t1, t2) -> {
-            if(t1.end == t2.end){
-                return t1.start - t2.start;
-            };
-            return t1.end - t2.end;
+        Arrays.sort(graph, (v1, v2) -> {
+            if(v1.end  == v2.end){
+                return v1.start - v2.start;
+            }
+            return v1.end - v2.end;
         });
-        int max = 1;
-        int end = meets[0].end;
-        for(int i=1; i<meets.length; i++){
-            if(meets[i].start >= end){
-                max++;
-                end = meets[i].end;
+
+        int end = graph[0].end;
+
+        for(int i=1; i<graph.length; i++){
+            if(graph[i].start >= end){
+                end = graph[i].end;
+                ans++;
             }
         }
 
-        System.out.println(max);
 
+        System.out.println(ans);
     }
 
-    private static class Meet{
+    private static class Node{
         public int start;
         public int end;
 
-        public Meet(int start, int end){
+        public Node(int start, int end){
             this.start = start;
             this.end = end;
         }
