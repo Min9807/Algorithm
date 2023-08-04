9fe5eb60f4baf4848aac7ba277fe4b2cdf287e38
MinJeong <97437403+Min9807@users.noreply.github.com>
2023-08-04 13:38:01 +0900

    [Gold IV] Title: 단어 수학, Time: 128 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -8,27 +8,24 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         int[] alphabet = new int[26];
-        String[] arr = new String[N];
 
-        while(N > 0){
-            N--;
-            arr[N] = br.readLine();
-            int len = arr[N].length();
-            int pow = (int)Math.pow(10, len-1);
-            for(int i=0; i<len; i++){
-                alphabet[arr[N].charAt(i) - 'A'] += pow;
-                pow /= 10;
+        for(int i=0; i<N; i++){
+            String str = br.readLine();
+            int len =str.length();
+            int value = (int)Math.pow(10, len-1);
+            for(int j=0; j<len; j++){
+                alphabet[str.charAt(j) - 'A'] += value;
+                value /= 10;
             }
         }
-
         Arrays.sort(alphabet);
+        int sum =0;
+        int value = 9;
 
-        int sum = 0;
-        int num = 9;
         for(int i=25; i>=0; i--){
             if(alphabet[i] == 0) break;
-            sum += alphabet[i] * num;
-            num--;
+            sum += alphabet[i] * value;
+            value--;
         }
 
         System.out.println(sum);
