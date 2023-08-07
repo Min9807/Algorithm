2f50dd30aa1924045d789bab61856f27bb10c4cc
MinJeong <97437403+Min9807@users.noreply.github.com>
2023-08-07 15:20:15 +0900

    [Gold IV] Title: 수 묶기, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -1,50 +1,55 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
 
+        int N = Integer.parseInt(br.readLine());
         List<Integer> plus = new ArrayList<>();
         List<Integer> minus = new ArrayList<>();
         int zeroCnt = 0;
+        int ans = 0;
 
         for(int i=0; i<N; i++){
             int num = Integer.parseInt(br.readLine());
-            if(num > 0) plus.add(num);
-            else if (num < 0) minus.add(num);
-            else zeroCnt++;
+            if(num > 0){
+                plus.add(num);
+            }else if(num < 0){
+                minus.add(num);
+            }else{
+                zeroCnt++;
+            }
         }
 
         Collections.sort(plus, Collections.reverseOrder());
         Collections.sort(minus);
-
-        int plusIdx = 0;
-        int minusIdx = 0;
-        long result = 0;
-
-        while(plusIdx + 1 < plus.size()){
-            if(plus.get(plusIdx) == 1 || plus.get(plusIdx + 1) == 1) {
-                result += plus.get(plusIdx) + plus.get(plusIdx + 1);
-                plusIdx += 2;
-            }else {
-                result += plus.get(plusIdx) * plus.get(plusIdx + 1);
-                plusIdx += 2;
+        for(int i=0; i< plus.size(); i++){
+            if(i != plus.size()-1){
+                if(plus.get(i) == 1 || plus.get(i+1) == 1){
+                    ans += plus.get(i) + plus.get(i+1);
+                }else{
+                    ans += plus.get(i) * plus.get(i+1);
+                }
+                i++;
+            }else{
+                ans += plus.get(i);
             }
         }
-        if(plusIdx < plus.size()){
-            result += plus.get(plusIdx);
+
+        for(int i=0; i< minus.size(); i++){
+            if(i == minus.size()-1 && minus.size() % 2 == 1){
+                if(zeroCnt > 0){
+                    break;
+                }else{
+                    ans += minus.get(i);
+                }
+            }else{
+                ans += minus.get(i) * minus.get(i+1);
+                i++;
+            }
         }
 
-        while(minusIdx +1 < minus.size()){
-            result += minus.get(minusIdx) * minus.get(minusIdx + 1);
-            minusIdx += 2;
-        }
-        if(minusIdx < minus.size() && zeroCnt == 0){
-            result += minus.get(minusIdx);
-        }
-
-        System.out.println(result);
+        System.out.println(ans);
     }
 }
