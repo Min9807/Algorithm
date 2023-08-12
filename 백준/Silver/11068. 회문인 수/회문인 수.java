8be68cd41dddf50fdd14cde288d18a6a6f6a626d
MinJeong <97437403+Min9807@users.noreply.github.com>
2023-08-12 19:18:14 +0900

    [Silver V] Title: 회문인 수, Time: 172 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Silver/11068. 회문인 수/회문인 수.java
+++ 백준/Silver/11068. 회문인 수/회문인 수.java
@@ -1,31 +1,38 @@
-import java.util.Scanner;
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
-        for (int t = 0; t < T; t++) {
-            long n = scanner.nextLong();
-            System.out.println(isPalindromeInAnyBase(n));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while(T-- > 0){
+            String str = br.readLine();
+            int check = calcu(str);
+            System.out.println(check);
         }
     }
-
-    public static int isPalindromeInAnyBase(long n) {
-        for (int B = 2; B <= 64; B++) {
-            if (isPalindrome(n, B)) {
+    private static int calcu(String str){
+        StringBuilder sb = new StringBuilder(str);
+        if(str.equals(sb.reverse().toString())){
+            return 1;
+        }
+        for(int i=2; i<65; i++){
+            int num = Integer.parseInt(str);
+            sb = new StringBuilder();
+            while(num > 0){
+                int a = num%i;
+                if(a < 10) sb.append(a);
+                else{
+                    sb.append((char)('A' + a - 10));
+                }
+                num/=i;
+            }
+            String word = sb.toString();
+            if(word.equals(sb.reverse().toString())){
                 return 1;
             }
         }
         return 0;
-    }
-
-    public static boolean isPalindrome(long n, int B) {
-        long reversed = 0;
-        long temp = n;
-        while (temp > 0) {
-            reversed = reversed * B + temp % B;
-            temp /= B;
-        }
-        return reversed == n;
     }
 }
