466d7a685c7f453b3bba6c68ac6093578fea8fbf
MinJeong <97437403+Min9807@users.noreply.github.com>
2023-08-13 16:10:51 +0900

    [Silver III] Title: 빈도 정렬, Time: 212 ms, Memory: 16376 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -1,60 +1,75 @@
 import java.util.*;
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        long C = sc.nextLong();
-        List<Number> list = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        long C = Integer.parseInt(st.nextToken());
+        List<Node> list = new ArrayList<>();
         int order = 1;
+        st = new StringTokenizer(br.readLine());
         while(N-- > 0){
-            long num = sc.nextLong();
+            long num = Long.parseLong(st.nextToken());
             boolean check = true;
-            for(Number number : list){
-                if(number.num == num){
-                    number.increaseCnt();
-                    check =false;
+            for(Node node : list){
+                if(node.value == num){
+                    node.increase();
+                    check = false;
                     break;
                 }
             }
             if(check){
-                list.add(new Number(num, order++, 1));
+                list.add(new Node(num, order++, 1));
             }
         }
-
-        Collections.sort(list, (t1, t2) ->{
-            if(t1.cnt == t2.cnt){
-                return t1.order - t2.order;
-            }
-            return t2.cnt - t1.cnt;
-        });
-
-        for(Number n : list){
-            long value = n.num;
+        
+        Collections.sort(list);
+        for(Node n : list){
+            long value = n.value;
             int cnt = n.cnt;
             while(cnt-- > 0){
                 System.out.print(value + " ");
             }
         }
-
-
     }
-    private static class Number{
-        public long num;
+
+    private static class Node implements Comparable<Node>{
+        public long value;
         public int order;
         public int cnt;
-
-        public Number(long num, int order, int cnt){
-            this.num = num;
+        
+        public Node(long value, int order, int cnt){
+            this.value = value;
             this.order = order;
             this.cnt = cnt;
         }
-
-        public void increaseCnt(){
+        
+        public void increase(){
             this.cnt += 1;
+        }
+
+        @Override
+        public int compareTo(Node n){
+            if(n.cnt == cnt){
+                return order - n.order;
+            }
+            return n.cnt - cnt;
+        }
+
+        @Override
+        public boolean equals(Object o){
+            if(o == this) return true;
+            if(!(o instanceof Node)) return false;
+            Node node = (Node) o;
+            return Objects.equals(value, node.value);
+        }
+
+        @Override
+        public int hashCode(){
+            return Objects.hash(value);
         }
     }
 }
