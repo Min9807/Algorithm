9e3481a377ddfbd23de3860564b394d9618e26e6
MinJeong <97437403+Min9807@users.noreply.github.com>
2023-08-30 14:15:28 +0900

    [level 3] Title: 최고의 집합, Time: 0.01 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, int s) {
+        if(n > s){
+            return new int[]{-1};
+        }
+        int[] answer = new int[n];
+        
+        int min = s/n;
+        int maxIdx = s%n;
+        
+        for(int i=0; i<n; i++){
+            answer[i] = min;
+        }
+        
+        for(int i=0; i<maxIdx; i++){
+            answer[n-i-1] += 1;
+        }
+        
+        return answer;
+    }
+}
