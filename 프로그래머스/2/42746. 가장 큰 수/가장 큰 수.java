8812e718d7fea08d5ba8f4d11c4c68f2572c599d
MinJeong <97437403+Min9807@users.noreply.github.com>
2023-10-14 11:37:38 +0900

    [level 2] Title: 가장 큰 수, Time: 1.70 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String[] strNumbers = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++){
+            strNumbers[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(strNumbers, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        if (strNumbers[0].equals("0")){
+            return "0";
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for (String str : strNumbers){
+            answer.append(str);
+        }
+
+        return answer.toString();
+    }
+}
