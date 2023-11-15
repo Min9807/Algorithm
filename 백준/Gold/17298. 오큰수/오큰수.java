b9d0a89068843bfb90ac317492052459a050e66a
MinJeong <97437403+Min9807@users.noreply.github.com>
2023-11-15 11:43:24 +0900

    [Gold IV] Title: 오큰수, Time: 1088 ms, Memory: 150572 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,33 +1,32 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        int[] B = new int[N];
-        Arrays.fill(B, -1);  // B 배열을 -1로 초기화
 
-        StringTokenizer stz = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(stz.nextToken());
+        Stack<Integer> stack = new Stack<>();
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Stack<Integer> st = new Stack<>();
-        for(int i=0; i<N; i++){
-            while(!st.empty() && A[st.peek()] < A[i]){
-                B[st.pop()] = A[i];
+        stack.push(0);
+        int[] answer = new int[N];
+        Arrays.fill(answer, -1);
+        for(int i=1; i<arr.length; i++) {
+            while(!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                answer[stack.peek()] = arr[i];
+                stack.pop();
             }
-            st.push(i);
+            stack.push(i);
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int num : B) {
-            sb.append(num + " ");
+        for(int num : answer) {
+            bw.write(num + " ");
         }
-        bw.write(sb.toString().trim());
         bw.flush();
     }
 }
