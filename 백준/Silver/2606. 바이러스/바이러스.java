b861f1d9bc879458fb4d8268ac5898344264f541
MinJeong <97437403+Min9807@users.noreply.github.com>
2023-11-16 11:48:09 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,34 +1,38 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int[][] computers;
     static boolean[] visited;
-    static int count = 0;
+    static int[][] graphs;
+    static int answer = -1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void dfs(int start, int n) {
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        graphs = new int[N+1][N+1];
+        visited = new boolean[N+1];
+
+        for(int i=0; i<M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+            graphs[num1][num2] = 1;
+            graphs[num2][num1] = 1;
+        }
+
+        dfs(1);
+
+        System.out.println(answer);
+    }
+    private static void dfs(int start) {
+        answer++;
         visited[start] = true;
-        for (int i = 1; i <= n; i++) {
-            if (computers[start][i] == 1 && !visited[i]) {
-                count++;
-                dfs(i, n);
+        for(int i=1; i< visited.length; i++) {
+            if(!visited[i] && graphs[start][i] == 1) {
+                dfs(i);
             }
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();  // 컴퓨터의 수
-        int m = sc.nextInt();  // 네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수
-        computers = new int[n+1][n+1];
-        visited = new boolean[n+1];
-
-        for (int i = 0; i < m; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            computers[a][b] = computers[b][a] = 1;
-        }
-
-        dfs(1, n);
-        System.out.println(count);
-    }
 }
