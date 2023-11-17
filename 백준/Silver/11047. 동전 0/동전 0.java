a7a253a4913e721c5e5b7308a5d47a53b0a454fb
MinJeong <97437403+Min9807@users.noreply.github.com>
2023-11-17 23:35:18 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,27 +1,23 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        int ans = 0;
-        int[] arr = new int[N];
-        for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+        int target = Integer.parseInt(st.nextToken());
+        int[] coin = new int[N];
+        for(int i=0; i<N; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
         }
-        for(int i=N-1; i>=0; i--){
-            if(K / arr[i] > 0){
-                ans = ans + (K / arr[i]);
-                K = K % arr[i];
+
+        int cnt = 0;
+        for(int i=N-1; i>=0; i--) {
+            if(coin[i] <= target) {
+                cnt += target / coin[i];
+                target %= coin[i];
             }
         }
-        System.out.println(ans);
-
+        System.out.println(cnt);
     }
-
 }
