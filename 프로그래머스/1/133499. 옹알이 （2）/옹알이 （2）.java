c3ec01361a0931aee6ba88915d4562da38c7ccdb
MinJeong <97437403+Min9807@users.noreply.github.com>
2023-12-12 17:54:45 +0900

    [level 1] Title: 옹알이 (2), Time: 0.52 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(String babble : babbling) {
+            if(babble.contains("ayaaya") || babble.contains("yeye") || babble.contains("woowoo") || babble.contains("mama")) continue;
+            
+            babble = babble.replace("aya", " ");
+            babble = babble.replace("ye", " ");
+            babble = babble.replace("woo", " ");
+            babble = babble.replace("ma", " ");
+            babble = babble.trim();
+            if(babble.length() == 0) answer++;
+        }
+        return answer;
+    }
+}
