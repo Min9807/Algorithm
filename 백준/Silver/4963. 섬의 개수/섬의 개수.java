db4093ced0444b58398787d1b6dde61a27da1011
MinJeong <97437403+Min9807@users.noreply.github.com>
2024-01-31 20:40:29 +0900

    [Silver II] Title: 섬의 개수, Time: 176 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,57 +1,50 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int[] dx = {1, -1 ,0 ,0, 1, -1, 1, -1};
-    static int[] dy = {0, 0 , 1,-1, 1, 1, -1, -1};
-
+    static int[][] graphs;
+    static int[] dx = {0, 0, 1, -1, 1, -1, 1, -1};
+    static int[] dy = {1, -1, 0, 0, 1, -1, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while(true){
+        while(true) {
+            int answer = 0;
             StringTokenizer st = new StringTokenizer(br.readLine());
             int w = Integer.parseInt(st.nextToken());
             int h = Integer.parseInt(st.nextToken());
+            if(w == 0 && h == 0) break;
 
-            if(w == 0 && h == 0){
-                break;
-            }
-
-            int[][] maps = new int[h][w];
-            for(int i=0; i<h; i++){
+            graphs = new int[h][w];
+            for(int i=0; i<h; i++) {
                 st = new StringTokenizer(br.readLine());
-                for(int j=0; j<w; j++){
-                    maps[i][j] = Integer.parseInt(st.nextToken());
+                for(int j=0; j<w; j++) {
+                    graphs[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            int cnt = 0;
-
-            for(int i=0; i<h; i++){
-                for(int j=0; j<w; j++){
-                    if(maps[i][j] == 1){
-                        cnt++;
-                        dfs(j, i, w, h, maps);
+            for(int i=0; i<h; i++) {
+                for(int j=0; j<w; j++) {
+                    if(graphs[i][j] == 1) {
+                        answer++;
+                        dfs(j, i);
                     }
                 }
             }
-
-            bw.write(cnt + "\n");
+            System.out.println(answer);
         }
-        bw.flush();
     }
-
-    private static void dfs(int x, int y, int w, int h, int[][] maps){
-        maps[y][x] = 0;
-        for(int i=0; i<8; i++){
+    private static void dfs(int x, int y) {
+        graphs[y][x] = 0;
+        for(int i=0; i<8; i++) {
             int kx = x + dx[i];
             int ky = y + dy[i];
-            if(kx >= 0 && kx < w && ky >= 0 && ky < h){
-                if(maps[ky][kx] == 1){
-                    dfs(kx, ky, w, h, maps);
+            if(kx >= 0 && kx < graphs[0].length && ky >= 0 && ky < graphs.length) {
+                if(graphs[ky][kx] == 1) {
+                    dfs(kx, ky);
                 }
             }
         }
     }
+
 }
